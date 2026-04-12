d1972539dbea639f8c57c8b6aef6db30ca22e2f0
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-12 20:36:09 +0530

    Add logout file

--- Career Guidence Project/logout.php
+++ Career Guidence Project/logout.php
@@ -9,7 +9,7 @@ session_start();
 
 // Unset all session variables
 $_SESSION = array();
-
+ 
 // Destroy the session cookie
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time() - 3600, '/');

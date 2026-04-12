c9d01047fa1e4e93a9e1cfaf520e6990b8c7417b
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-12 20:37:10 +0530

    Add save data file

--- Career Guidence Project/save_data.php
+++ Career Guidence Project/save_data.php
@@ -11,7 +11,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
 // Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if ($_SERVER['REQUEST_METHOD'] ===  'OPTIONS') {
     http_response_code(200);
     exit();
 }
